c244ee8cb6253b236f16279432d4c94882cdd5a6
nclsang <sj94331717@gmail.com>
2024-06-01 17:30:28 +0900

    Update jsp.js

--- jsp.js
+++ jsp.js
@@ -1,4 +1,7 @@
-// app.js
+//jsp.js
+
+
+//page1를 클릭했을 때 실행되는 함수//
 function page1Click() {
     const intro =
         document.getElementById('page1');
@@ -9,9 +12,18 @@ function page1Click() {
     const two =
         document.getElementById('page3');
     two.style.display = 'none';
-
-
+    const three =
+        document.getElementById('page4');
+    three.style.display = 'none';
+    // Header의 배경색을 검정색으로 설정
+    const header = document.getElementById('header');
+    header.style.backgroundColor = 'black';
+    // SJ Jeon 글자색를 white로 설정
+    const button = document.getElementById('button');
+    button.style.color = 'white';
 }
+
+//page2를 클릭했을 때 실행되는 함수//
 function page2Click() {
     const intro =
         document.getElementById('page1');
@@ -22,7 +34,21 @@ function page2Click() {
     const two =
         document.getElementById('page3');
     two.style.display = 'none';
+    const three =
+        document.getElementById('page4');
+    three.style.display = 'none';
+    // Header의 배경색 투명도를 0으로 설정
+    const header = document.getElementById('header');
+    header.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    // SJ Jeon 글자색를 gold로 설정
+    const button = document.getElementById('button');
+    button.style.color = 'gold';
+    // firstnavbar의 글자색를 gold로 설정
+    const firstnavbarcolor = document.getElementById('firstnavbarcolor');
+    firstnavbarcolor.style.color = 'gold';
 }
+
+//page3를 클릭했을 때 실행되는 함수//
 function page3Click() {
     const intro =
         document.getElementById('page1');
@@ -33,8 +59,40 @@ function page3Click() {
     const two =
         document.getElementById('page3');
     two.style.display = 'block';
+    const three =
+        document.getElementById('page4');
+    three.style.display = 'none';
+    // Header의 배경색을 검정색으로 설정
+    const header = document.getElementById('header');
+    header.style.backgroundColor = 'black';
+    // SJ Jeon 글자색를 white로 설정
+    const button = document.getElementById('button');
+    button.style.color = 'white';
 }
 
+//page4를 클릭했을 때 실행되는 함수//
+function page4Click() {
+    const intro =
+        document.getElementById('page1');
+    intro.style.display = 'none';
+    const one =
+        document.getElementById('page2');
+    one.style.display = 'none';
+    const two =
+        document.getElementById('page3');
+    two.style.display = 'none';
+    const three =
+        document.getElementById('page4');
+    three.style.display = 'block';
+    // Header의 배경색을 검정색으로 설정
+    const header = document.getElementById('header');
+    header.style.backgroundColor = 'black';
+    // SJ Jeon 글자색를 white로 설정
+    const button = document.getElementById('button');
+    button.style.color = 'white';
+}
+
+//페이지가 로드될 때 기본적으로 page1을 클릭하게 함//
 window.onload = page1Click;
 
 
